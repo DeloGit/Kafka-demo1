7d04a11f3ed93a3fb98976ae1da5065dccadf817
Sorin Maier <sorinmaier@MacBook-Pro.localdomain>
2021-02-15 09:48:46 -0500

    First commit

---
+++ src/test/java/Kafka/demo1/Kafkademo1/KafkaDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Kafka.demo1.Kafkademo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
